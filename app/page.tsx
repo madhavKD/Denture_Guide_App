1fb5ec115171eeece7562e0cbf897d61aae6ea69
Micha Mailänder <micha.mailaender@dechea.com>
2022-12-27 14:05:15 +0000

    First try of teeth diagram compositions

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import SelectProduct from "../components/SelectProduct";
 import FetchUsers from "../components/FetchUsers";
 import { View } from "../components/Reshaped/Reshaped";
+import TeethDiagram from "../components/teeth-diagram";
 
 export default function Home() {
   return (
     <View>
       <FetchUsers />
       <SelectProduct />
+      <TeethDiagram />
     </View>
   )
 }
