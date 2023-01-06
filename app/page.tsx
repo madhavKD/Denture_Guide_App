0cdcef81d106386989f02a3f5abfef003fd14f1f
Micha Mailänder <micha.mailaender@dechea.com>
2023-01-06 12:59:59 +0000

    Cleaned up main

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-import SelectProduct from "../components/SelectProduct";
 import { View } from "../components/Reshaped/Reshaped";
-import Tooth from "../components/Tooth";
 import FetchUsers from "../components/FetchUsers";
-import Intellisense from "../components/Intellisense";
 
 export default function Home() {
   return (
     <View>
       {/* <FetchUsers /> */}
-      <SelectProduct />
-      <Tooth />
-      <Intellisense />
     </View>
   )
 }
