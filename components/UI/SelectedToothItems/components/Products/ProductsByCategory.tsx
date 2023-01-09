4635e05951823ba26a08ab945cc88b3b5e001f09
Madhav <madhav.kabra@dechea.com>
2023-01-09 13:12:47 +0000

    Update condition in product list

--- components/UI/SelectedToothItems/components/Products/ProductsByCategory.tsx
+++ components/UI/SelectedToothItems/components/Products/ProductsByCategory.tsx
@@ -6,36 +6,43 @@ import selectedToothData from "../../../SelectedTooth/data.json";
 
 export default function ProductsByCategory({ params }: { params: any }) {
   const subItem = params['sub-item'];
+  const item = params.item;
+
 
   return (
     <>
       {
         selectedToothData[1].availableOptions?.map((availableOption) => (
-          availableOption.options?.map((option) => (
-            option?.url === subItem ? (
-              Boolean(option?.options?.length) ? (
-                <View padding={8} paddingTop={2}>
-                  <Tabs >
-                    <Tabs.List>
+
+          availableOption.url === item && availableOption.options?.map((option) => {
+            { console.log(option?.url, subItem) }
+            return (
+              option?.url === subItem ? (
+                Boolean(option?.options?.length) ? (
+                  <View padding={8} paddingTop={2}>
+                    <Tabs >
+                      <Tabs.List>
+                        {option.options?.map((subOption, subIndex) => (
+                          <Tabs.Item value={subOption.name} key={subIndex}>
+                            <Text variant="body-medium-2">{subOption.name}</Text>
+                          </Tabs.Item>
+                        ))}
+                      </Tabs.List>
                       {option.options?.map((subOption, subIndex) => (
-                        <Tabs.Item value={subOption.name} key={subIndex}>
-                          <Text variant="body-medium-2">{subOption.name}</Text>
-                        </Tabs.Item>
+                        <Tabs.Panel key={subIndex} value={subOption.name}>
+                          <ProductCards data={subOption?.products} />
+                        </Tabs.Panel>
                       ))}
-                    </Tabs.List>
-                    {option.options?.map((subOption, subIndex) => (
-                      <Tabs.Panel key={subIndex} value={subOption.name}>
-                        <ProductCards data={subOption?.products} />
-                      </Tabs.Panel>
-                    ))}
-                  </Tabs>
-                </View>
-              ) : (
-                <ProductCards data={option?.products} />
-              )
-            ) : null
-          ))
+                    </Tabs>
+                  </View>
+                ) : (
+                  <ProductCards data={option?.products} />
+                )
+              ) : null
+            )
+          })
         ))
+
       }
     </>
 
