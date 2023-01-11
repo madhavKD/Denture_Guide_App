bfc9be6d3f27d79ed5b850205ec448fc37f46e1e
Madhav Kabra <madhav.kabra@dechea.com>
2023-01-11 13:00:39 +0530

    Add hover effect

--- components/UI/SelectedTooth/SelectedTooth.tsx
+++ components/UI/SelectedTooth/SelectedTooth.tsx
@@ -1,32 +1,51 @@
-
 'use client';
 
 import { MenuItem, Text, View } from '../../Reshaped/Reshaped';
-import selectedToothData from "./data.json";
+import selectedToothData from './data.json';
 import { usePathname } from 'next/navigation';
-
-
-
+import Image from 'next/image';
 
 export default function SelectedTooth() {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
-
-    <View gap={2} paddingTop={5} direction='column' divided>
+    <View gap={2} paddingTop={5} direction="column" divided>
       {selectedToothData.map((tooth, index) => (
         <>
           <View key={index} gap={1}>
-            <MenuItem roundedCorners selected={pathname?.includes(`/selected-tooth/${tooth.url}`)} href={`/selected-tooth/${tooth.url}`}>
-              <Text variant='body-medium-2'>
-                {tooth.title}
-              </Text>
+            <MenuItem
+              roundedCorners
+              selected={pathname?.includes(`/selected-tooth/${tooth.url}`)}
+              href={`/selected-tooth/${tooth.url}`}
+            >
+              <Text variant="body-medium-2">{tooth.title}</Text>
             </MenuItem>
 
-            <View direction="column" gap={1} paddingStart={3} paddingTop={2} >
+            <View direction="column" gap={1} paddingStart={3} paddingTop={2}>
               {tooth.availableOptions.map((availableOption, index) => (
-                <MenuItem key={index} size="small" roundedCorners selected={pathname?.includes(`/selected-tooth/${tooth.url}/${availableOption.url}`)} href={`/selected-tooth/${tooth.url}/${availableOption.url}`} >
-                  <Text variant='body-medium-2' color='neutral-faded'>{availableOption.name}</Text>
+                <MenuItem
+                  key={index}
+                  size="small"
+                  roundedCorners
+                  selected={pathname?.includes(
+                    `/selected-tooth/${tooth.url}/${availableOption.url}`
+                  )}
+                  href={`/selected-tooth/${tooth.url}/${availableOption.url}`}
+                  className="menuItemLink"
+                  startIcon={
+                    'startIcon' in availableOption ? (
+                      <Image
+                        src={availableOption.startIcon}
+                        height={16}
+                        width={16}
+                        alt="Img"
+                      />
+                    ) : (
+                      <></>
+                    )
+                  }
+                >
+                  <Text variant="body-medium-2">{availableOption.name}</Text>
                 </MenuItem>
               ))}
             </View>
