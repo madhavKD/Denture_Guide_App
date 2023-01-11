378ac95a28e9aea361a133a4ea83de19dfdb3eb1
Madhav <madhav.kabra@dechea.com>
2023-01-11 07:49:45 +0000

    Add radio button

--- components/UI/SelectedTooth/SelectedTooth.tsx
+++ components/UI/SelectedTooth/SelectedTooth.tsx
@@ -1,35 +1,37 @@
 'use client';
 
-import { MenuItem, Text, View } from '../../Reshaped/Reshaped';
+import { MenuItem, Text, View, Radio } from '../../Reshaped/Reshaped';
 import selectedToothData from './data.json';
-import { usePathname } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 
 export default function SelectedTooth() {
-  const pathname = usePathname();
+  const router = useRouter();
+  const params = useSearchParams();
+  const step = params.get('step')?.split('-')
+  const questionNum = Number(step?.[0]);
+  const optionNum = Number(step?.[1])
 
   return (
     <View gap={2} paddingTop={5} direction="column" divided>
-      {selectedToothData.map((tooth, index) => (
+      {selectedToothData.map((tooth, toothIndex) => (
         <>
-          <View key={index} gap={1}>
+          <View key={toothIndex} gap={1}>
             <MenuItem
               roundedCorners
-              selected={pathname?.includes(`/selected-tooth/${tooth.url}`)}
+              selected={toothIndex <= questionNum}
               href={`/selected-tooth/${tooth.url}`}
             >
               <Text variant="body-medium-2">{tooth.title}</Text>
             </MenuItem>
 
             <View direction="column" gap={1} paddingStart={3} paddingTop={2}>
-              {tooth.availableOptions.map((availableOption, index) => (
+              {tooth.availableOptions.map((availableOption, optionIndex) => (
                 <MenuItem
-                  key={index}
+                  key={optionIndex}
                   size="small"
                   roundedCorners
-                  selected={pathname?.includes(
-                    `/selected-tooth/${tooth.url}/${availableOption.url}`
-                  )}
+                  selected={(toothIndex <= questionNum && optionIndex <= optionNum) || toothIndex < questionNum}
                   href={`/selected-tooth/${tooth.url}/${availableOption.url}`}
                   className="menuItemLink"
                   startIcon={
@@ -55,3 +57,16 @@ export default function SelectedTooth() {
     </View>
   );
 }
+
+
+
+{/* <View key={optionIndex}>
+                  <Radio
+                    value={`${availableOption.name}-${optionIndex}-${toothIndex}`}
+                    name={`${availableOption.name}-${optionIndex}-${toothIndex}`}
+                    checked={(toothIndex <= questionNum && optionIndex <= optionNum) || toothIndex < questionNum}
+                    onChange={() => router.push(`/selected-tooth/${tooth.url}/${availableOption.url}?step=${toothIndex}-${optionIndex}`)}
+                  >
+                    {availableOption.name}
+                  </Radio>
+                </View> */}
--- components/UI/SelectedToothItems/index.tsx
+++ components/UI/SelectedToothItems/index.tsx
@@ -5,21 +5,32 @@ import { View } from '../../Reshaped/Reshaped';
 import { Treatments } from './components/Treatments';
 import { Products } from './components/Products';
 import selectedToothData from "../SelectedTooth/data.json";
+import { useRouter } from 'next/navigation';
 
 export function SelectedToothItems({ params }: { params: any }) {
-  const item = params.item; 
+  const router = useRouter()
+  const section = params.section;
+  const item = params.item;
+
+  const handleSelectItem = (questionIndex: number, optionIndex: number) => {
+    if (selectedToothData[questionIndex].availableOptions.length - 1 <= optionIndex) {
+      router.push(`/selected-tooth/${selectedToothData[questionIndex + 1].url}/${selectedToothData[questionIndex + 1].availableOptions[0].url}?step=${questionIndex + 1}-0`)
+    } else {
+      router.push(`/selected-tooth/${selectedToothData[questionIndex].url}/${selectedToothData[questionIndex].availableOptions[optionIndex + 1].url}?step=${questionIndex}-${optionIndex + 1}`, { forceOptimisticNavigation: true})
+    }
+  }
 
   return (
     <View gap={2} align='stretch' paddingTop={2}>
-      {selectedToothData.map((tooth, index) => (
-        <View key={index} paddingTop={2}>
-          {tooth.availableOptions.map((availableOption, index) => (
-            <View key={index}>
+      {selectedToothData.map((tooth, toothIndex) => (
+        <View key={toothIndex} paddingTop={2}>
+          {tooth.availableOptions.map((availableOption, optionIndex) => (
+            <View key={optionIndex}>
               {availableOption.url === item && (
                 <>
                   {
                     tooth.type === 'treatment' && 'question' in availableOption && (
-                      <Treatments question={availableOption.question} title={availableOption.title} options={availableOption.options} />
+                      <Treatments onSelectItem={() => handleSelectItem(toothIndex, optionIndex)} question={availableOption.question} title={availableOption.title} options={availableOption.options} />
                     )
                   }
                   {
