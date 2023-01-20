c96d9ecf57fcf434450b49eb0b87e986e35eeaec
Madhav <madhav.kabra@dechea.com>
2023-01-20 13:44:46 +0000

    Comment edge runtime flag

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    runtime: 'experimental-edge',  },
+    // runtime: 'experimental-edge',
+  },
 }
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
